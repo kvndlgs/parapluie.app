9e32be5468c6e9e9d3cbb0f636ff3410bd8de89a
kvndlgs <poissedonn@gmail.com>
2025-08-21 22:36:01 +0000

    a fresh start

--- app/colors/page.tsx
+++
@@ -1,9 +0,0 @@
-import ColorPreview from '@/components/color-preview'
-
-export default function ColorsPage() {
-  return (
-    <div className="min-h-screen bg-background">
-      <ColorPreview />
-    </div>
-  )
-}
--- components/feature-section.tsx
+++
@@ -1,7 +0,0 @@
-export default function FeatureSection() {
-    return (
-        <section className="h-screen w-screen bg-primary flex items-center justify-center">
-            <h1> FEATURES </h1>
-        </section>
-    )
-}
