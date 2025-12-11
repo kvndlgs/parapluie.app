8be5ce7c670e779111f6179d3fdc0c5c70e775d9
kvndlgs <poissedonn@gmail.com>
2025-12-11 12:59:25 -0500

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ResponsiveNavbar from './components/ResponsiveNavbar';
-import { Hero } from './components/Hero';
-import { FeatureCarousel } from './components/FeatureCarousel';
-import { HowItWork } from './sections/howitwork';
-import Subscriptions from './sections/subscriptions';
-import  { SubscriptionsPage } from './pages/Subscriptions';
-import { AboutPage } from './pages/About';
-import { Footer } from './components/footer';
-import ArnaqueGrandParent from './pages/ArnaqueGrandParent';
-import ProtectionParents from './pages/ProtectionParents';
-import { Blog } from './pages/Blog';
-import { About } from './sections/about.tsx'
-import { Support } from './pages/Support'
-import { Media } from './pages/Media';
-import { FAQ } from './pages/FAQ';
-import BlogPost from './pages/BlogPost';
-import { Contact } from './pages/Contact';
-
-
-function HomePage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
+// src/App.tsx
+function App() {
   return (
-    <main>
-      <ResponsiveNavbar />
-      <Hero openModal={openModal} closeModal={closeModal} isModalOpen={isModalOpen} />
-      <section className="w-full relative z-20">
-        <FeatureCarousel />
-      </section>
-      <About />
-      <HowItWork />
-      <Subscriptions />
-      <Footer openModal={openModal} />
-    </main>
+    <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+      <h1>Parapluie is alive 🛡️☂️</h1>
+      <p>Si tu vois ce message en prod, React fonctionne.</p>
+    </div>
   );
 }
 
-function App() {
-  return (
-    
-<Routes>
-  {/* Marketing */}
+export default App;
+
+{/*
   <Route path="/" element={<HomePage />} />
   <Route path="/a-propos" element={<AboutPage />} />
   <Route path="/media" element={<Media />} />
@@ -52,12 +18,12 @@ function App() {
   <Route path="/protection-parents" element={<ProtectionParents />} />
   <Route path="/arnaque-grand-parent" element={<ArnaqueGrandParent />} />
 
-  {/* Support */}
+  {/* Support 
   <Route path="/support" element={<Support/>} />
   <Route path="/support/contact" element={<Contact />} />
   <Route path="/support/faq" element={<FAQ />} />
 
-  {/* Contenu */}
+  {/* Contenu 
   <Route path="/blog" element={<Blog />} />
   <Route path="/post/:slug" element={<BlogPost />} />
 </Routes>
@@ -65,3 +31,4 @@ function App() {
 }
 
 export default App;
+*/}
