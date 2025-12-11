aef0aec5a620a77c6275796a10dbcb0fcb21ea4e
kvndlgs <poissedonn@gmail.com>
2025-12-11 09:54:48 -0500

    update Media.tsx

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -3,11 +3,11 @@ import { Helmet } from 'react-helmet';
 export function Media() {
   return (
     <>
-      <Hemlet>
+      <Helmet>
         <title>
           Media - Parapluie
         </title>
-      </Hemlet>
+      </Helmet>
     <section>
       <h1> Media </h1>
     </section>
