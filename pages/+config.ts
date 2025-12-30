a94a1a515edc87402212371eca5d002c3d53ab75
kvndlgs <poissedonn@gmail.com>
2025-12-30 17:03:34 -0500

    feat: vike integration

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,5 +1,12 @@
 // pages/+config.ts
+
+import vikeReact from "vike-react/config"; // <--- AJOUTE CET IMPORT
+import type { Config } from "vike/types";
+
 export default {
+  // C'est cette ligne qui manquait !
+  extends: [vikeReact], 
+
   title: "Parapluie - Protection contre la fraude",
-  description: "Protection contre les arnaques au Québec"
-};
+  description: "Protection contre les arnaques au Québec",
+} satisfies Config;
