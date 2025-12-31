9bfd20d3487067481fd123320a5f3ae6a87c922a
kvndlgs <poissedonn@gmail.com>
2025-12-31 10:13:52 -0500

    update +config.ts

--- renderer/+config.ts
+++ renderer/+config.ts
@@ -1,4 +1,6 @@
-export default {
+const renderer = {
     partial: true,     // suppresses "non-static route" warnings
     noExtraDir: true   // generates /guide/slug/index.html instead of flat /slug.html
 };
+
+export default renderer;
