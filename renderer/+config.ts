3d8dd88d89c7011e8f987d37c57e699846f5ff22
kvndlgs <poissedonn@gmail.com>
2025-12-31 09:23:32 -0500

    update +config.ts

--- renderer/+config.ts
+++ renderer/+config.ts
@@ -1,6 +1,8 @@
+// renderer/+config.ts
+import type { Config } from 'vike/types';
+
 export default {
   prerender: {
-    partial: true,  // Allows some routes not prerendered
-    noExtraDir: true
+    partial: true  // Allows dynamic routes without warning if not all prerendered
   }
-};
+} satisfies Config;
