71e1ef8cd7df26a4d7f5b0812f9e27d8733cd7b2
kvndlgs <poissedonn@gmail.com>
2025-12-31 03:18:59 -0500

    update

--- pages/guide/@slug/prerender.ts
+++ pages/guide/@slug/prerender.ts
@@ -1,10 +1,12 @@
 // pages/guide/@slug/prerender.ts
 import { getAllGuides } from '../../../src/content/guides';
 
-export const prerender = async () => {
+const prerender = async () => {
   const guides = getAllGuides();
   
   // On ne génère des pages QUE pour les guides qui existent
   // Vike n'appellera jamais le +data.ts avec un mauvais slug pendant le build
   return guides.map(guide => `/guide/${guide.slug}`);
 };
+
+export default prerender;
--- pages/post/@slug/prerender.ts
+++ pages/post/@slug/prerender.ts
@@ -1,10 +1,12 @@
 // pages/guide/@slug/prerender.ts
 import { getAllPosts } from '../../../src/content/posts';
 
-export const prerender = async () => {
+const prerender = async () => {
   const posts = getAllPosts();
   
   // On ne génère des pages QUE pour les guides qui existent
   // Vike n'appellera jamais le +data.ts avec un mauvais slug pendant le build
   return posts.map(post => `/post/${post.slug}`);
 };
+
+export default prerender;
