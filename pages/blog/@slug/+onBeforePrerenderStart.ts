86eb362e5886af87412945d0314e06606288b41e
kvndlgs <poissedonn@gmail.com>
2026-01-03 14:15:17 -0500

    update

---
+++ pages/blog/@slug/+onBeforePrerenderStart.ts
@@ -0,0 +1,13 @@
+import postsData from '../../../src/content/posts.json';
+
+interface Post {
+  slug: string;
+}
+
+const onBeforePrerenderStart = () => {
+  const posts = postsData as Post[];
+  
+  return posts.map(post => `/post/${post.slug}`);
+};
+
+export default onBeforePrerenderStart;
---
+++ pages/guides/@slug/+onBeforePrerenderStart.ts
@@ -0,0 +1,13 @@
+import guidesData from '../../../src/content/guides.json';
+
+interface Guide {
+  slug: string;
+}
+
+const onBeforePrerenderStart = () => {
+  const guides = guidesData as Guide[];
+  
+  return guides.map(guide => `/guide/${guide.slug}`);
+};
+
+export default onBeforePrerenderStart;
