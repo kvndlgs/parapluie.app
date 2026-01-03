fd7907a593bf623d7105926834536ff2ccb4f693
kvndlgs <poissedonn@gmail.com>
2026-01-03 12:53:24 -0500

    update

--- pages/guides/@slug/+prerender.ts
+++ pages/guides/@slug/+prerender.ts
@@ -5,7 +5,5 @@ interface Guide {
 }
 
 export const prerender = async () => {
-  // On s'assure que guidesData est bien traité comme un tableau
-    // et on retourne la liste des URLs complètes
       return (guidesData as Guide[]).map(guide => `/guide/${guide.slug}`);
 };
