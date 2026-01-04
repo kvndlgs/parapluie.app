4a529ea11535d307cfc8ff8926d3834179762bd5
kvndlgs <poissedonn@gmail.com>
2026-01-04 17:36:57 -0500

    update

--- pages/+config.ts
+++ pages/+config.ts
@@ -5,7 +5,8 @@ import type { Config } from "vike/types";
 
 export default {
   lang: "fr",
-  title: "Parapluie - Protection contre la fraude.",
+  //title: "Parapluie - Protection contre la fraude.",
+  
   stream: true,
   extends: [vikeReact],
   prerender: true,
